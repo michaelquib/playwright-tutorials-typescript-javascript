eac7699ec8f2dc628a4e48bd030f14c6fa2b713f
michaelquib <michaelquiban@proton.me>
2025-11-12 18:33:03 -0800

    using own repo to show learning progress, installing playwright locally

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,101 +1,54 @@
 import { defineConfig, devices } from '@playwright/test';
-import dotenv from "dotenv"
-import path from 'path';
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// dotenv.config({
-//   path: process.env.TEST_ENV ? `./env-files/.env.${process.env.TEST_ENV}` : `./env-files/.env.dev`
-// })
-dotenv.config({
-  path: path.resolve('testdata','.env')//"./testdata/.env"
-})
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
 /**
  * See https://playwright.dev/docs/test-configuration.
- * 
  */
 export default defineConfig({
-
-
-  //grep:/UI/,
-  //grepInvert:/sanity/,
-  //globalSetup: "./global-setup.ts",
-  testDir: './tests',
-  // expect:{
-  //   toHaveScreenshot:{
-  //     maxDiffPixels:20,
-  //     //maxDiffPixelRatio:0.1
-  //   }
-  // },
+  testDir: './e2e',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 1 : 0,   
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  //reporter:[ ['html',{open:'on-failure'}]],
-  //reporter : [['junit',{outputFile:'xyz/report4junit.xml'}]],
-  reporter:[["html"],["github"]],
-  //reporter:process.env.CI?'github':'html',
-  //reporter:"allure-playwright",
-  //reporter:[["allure-playwright", {outputFolder:"GoogleAllureResults4"}],["html"]],
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-    baseURL:"https://restful-booker.herokuapp.com",
-    extraHTTPHeaders:{
-      Accept:"application/json",
-      "Content-Type":"application/json",
-      // Authorization: "Basic YWRtaW46cGFzc3dvcmQxMjM="
-    },
+    /* Base URL to use in actions like `await page.goto('')`. */
+    // baseURL: 'http://localhost:3000',
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless: false,
-    screenshot:'only-on-failure',
-    video:'retain-on-failure',
-    trace:'on',
-    //storageState:"./playwright/.auth/auth.json",
+    trace: 'on-first-retry',
   },
-  timeout: 30000,
+
   /* Configure projects for major browsers */
   projects: [
-    // {
-    //   name: "setup",
-    //   testMatch:/.*\.setup\.ts/,
-    //   //testMatch: "global.setup.ts"
-    // },
     {
       name: 'chromium',
-      //dependencies:["setup"],
-      use: {
-        ...devices['Desktop Chrome'],
-        //storageState: "./playwright/.auth/auth.json"
-      }
+      use: { ...devices['Desktop Chrome'] },
     },
-    // {
-    //   name: 'firefox',
-    //   //dependencies:["setup"],
-    //   use: {
-    //     ...devices['Desktop Firefox'],
-    //     //storageState: "./playwright/.auth/auth.json"
-    //   },
-    // },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
-    // {
-    //   name:'iphone14MaxPro',
-    //   use:{ ...devices['iPhone 14 Pro Max']}
-    // },
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
@@ -120,7 +73,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
+  //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
 });
