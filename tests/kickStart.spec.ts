4b34629a629f19d8152e1d671e8e82055de4e549
michaelquib <michaelquiban@proton.me>
2025-11-12 18:37:37 -0800

    Adding some messages for some sample tests

---
+++ tests/kickStart.spec.ts
@@ -0,0 +1,9 @@
+import {test} from "@playwright/test";
+
+test("Kickstarting my first test", () => {
+    console.log("Hello, Playwright!");
+})
+
+test("Kickstarting my second testing", () => {
+    console.log("Hello, Playwright again!");
+})
